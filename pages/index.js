7b37534b1d604afb9742652a888c7e514f04c2a2
EC2 Default User <ec2-user@ip-172-31-23-62.us-east-2.compute.internal>
2022-01-10 00:56:27 +0000

    adding bio

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import { Container, Box } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {Button, Container, Box, Heading, Image, useColorModeValue, Link } from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
 
 const Page = () => {
     return(
     <Container> 
-        <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
             Hello, I&apos;m a full-stack developer based in Central Florida! 
         </Box>
         
@@ -14,13 +18,40 @@ const Page = () => {
                 </Heading>
                 <p>Digital Craftsman ( Developer / Designer ) </p>
             </Box>
-            <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align="center">
-                <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid"
-                    maxWidth="100px" display="inline-block" borderRadius="full" 
-                    src="/images/takuya.jpg" alt="Profile Image"
+            <Box flexShrink={0}
+                mt={{base: 4, md: 0}}
+                ml={{md: 6}} 
+                align="center"
+            >
+                <Image borderColor="whiteAlpha.800"
+                    borderWidth={2}
+                    borderStyle="solid"
+                    maxWidth="100px"
+                    display="inline-block" 
+                    borderRadius="full" 
+                    src="/images/takuya.jpg"
+                    alt="Profile Image"
                 />
             </Box>
         </Box>
+        
+        <Section delay={0.1}>
+            <Heading as="h3" variant="section-title">
+            Work 
+            </Heading>
+            <Paragraph>Michael Da Silva is a freelance and a full-stack developer 
+            based in Central Florida with a passion for building digital services/stuff he wants. 
+            He has a knack for all things launching products, from planning and designing all the way 
+            to solving real-life problems with code. When not online, he loves hanging out with his camera. 
+            </Paragraph>
+            <Box align="center" my={4}>
+                <NextLink href="/works">
+                     <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                      My Portfolio
+                     </Button>
+                </NextLink>
+            </Box>
+        </Section>
     </Container>
     )
 }
