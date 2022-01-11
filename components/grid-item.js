d99fe40b478a2736964439b2af0d518d754fa84a
EC2 Default User <ec2-user@ip-172-31-23-62.us-east-2.compute.internal>
2022-01-11 23:06:44 +0000

    added works page

--- components/grid-item.js
+++ components/grid-item.js
@@ -20,3 +20,28 @@ export const GridItem = ({ children, href, title, thumbnail }) => (
         </LinkBox>
     </Box>
 )
+
+export const WorkGridItem = ({children, id, title, thumbnail }) => (
+    <Box w="100%" align="center">
+        <NextLink href={`/works/${id}`}>
+            <LinkBox cursor="pointer">
+              <Image src={thumbnail} alt={title} className="grid-item-thumbnail" placeholder="blur" />
+              <LinkOverlay href={`/works/${id}`}>
+                <Text mt={2} fontSize={20}>
+                    {title}
+                </Text>
+              </LinkOverlay>
+              <Text fontSize={14}> {children} </Text>
+            </LinkBox>
+        </NextLink>
+    </Box> 
+)
+
+export const GridItemStyle = () => (
+    <Global styles = {`
+        .grid-item-thumbnail {
+            border-radius: 12px;    
+        }
+        `}
+    />
+)
--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,9 @@
 import { Container, Heading, SimpleGrid, Divider} from '@chakra-ui/react'
 import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+
+import thumbTest from '../public/images/works/workTest.jpg'
+import thumbWalknote from '../public/images/works/testTwo.jpg'
 
 const Works = () => {
     return (
@@ -7,6 +11,26 @@ const Works = () => {
             <Heading as="h3" fontSize={20} mb={4}>
                 Works
             </Heading>
+            
+            <SimpleGrid columns={[1,1,2]} gap={6}>
+                <Section>
+                <WorkGridItem id="test" title="test" thumbnail={thumbTest} >
+                    Testing work section
+                </WorkGridItem>
+                </Section>
+                
+                <Section> 
+                    <WorkGridItem id="walknote" title="walknote" thumbnail={thumbWalknote}>
+                        Another Test, editing later
+                    </WorkGridItem>
+                </Section>
+                
+                <Section delay={0.3}> 
+                    <WorkGridItem id="test3" title="test3" thumbnail={thumbWalknote}>
+                        Another Test, editing later
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
         </Container>
     )
 }
